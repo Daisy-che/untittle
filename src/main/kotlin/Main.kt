fd1a9148ff8ad3d05962de0b6ae7f073a2636c21
Student <daisychepkiruih@gmail.com>
2024-03-25 08:27:47 +0300

    add file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    sentence("Barnie bakes brown bagels and burns")
+ println(arr(arrayOf(1,2,3)))
+    volume(3.14*4/3)
+    isPalindrome("madam","wow","kayak")
+
+}
+fun sentence(sentence:String){
+   // sentence.filterIndexed { index, c ->  }
+    //idx=index{0}
+    }
+//println(sentence
+
+fun arr(number:Array<Int>): Any {
+    return(number.sum())
+    return(number.count())
+    return(number.average())
+}
+fun volume (volumeOfSphere:Double){
+    println(volumeOfSphere)
+}
+fun isPalindrome(words: String, s: String, s1: String) {
+   var sentence=words
+    println(sentence)
+}
